3c0aadd61fde75120914b2902e69595b2b93fb19
Lily Clark <lilyclarkcodes@gmail.com>
2022-11-22 14:04:08 -0800

    decided to use ejs

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
-require('dotenv').config()
 
-app.listen(process.env.PORT || PORT, () => {
-    console.log(`server running on port ${PORT}`)
-})
+
 
 let db,
-    dbConnectionStr = process.env.DB_STRING,
+    dbConnectionStr = '//put in the route',
     dbName = 'score'
 
 MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
@@ -23,9 +20,16 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-app.get('/', async (request, response)=>{
-    const currentScore = await db.collection('score').find()
-    response.render('index.ejs', {})
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`server running on port ${PORT}`)
+})
+
+app.get('/', (request, response)=>{
+    db.collection('score').find()
+    .then(data => {
+        response.render('index.ejs', {info: score})
+    })
+    .catch(error => console.error(error))
 })
 
 app.get('/api', (request, response)=>{
@@ -33,4 +37,13 @@ app.get('/api', (request, response)=>{
     let diceRollResTwo = Math.floor(Math.random()*12)
     let productRoll = diceRollResOne*diceRollResTwo
     response.
+})
+
+app.post('/updateScore', (request, response) => {
+    db.collection('score').insertOne(request.body.score)
+    .then(result => {
+        console.log('New Score Recorded')
+        response.redirect('/')
+    .catch(error => console.error(error))
+    })
 })
